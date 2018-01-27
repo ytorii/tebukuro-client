1318b66092e38e96f0007f3e32075804683f655b
ytorii <metalwarrior_type_jp@hotmail.com>
2018-01-27 18:55:57 +0900

    Add check authenticated by setInterval

--- actions/user.js
+++ actions/user.js
@@ -3,37 +3,62 @@ import { createAction } from 'redux-actions'
 import type { Dispatch } from 'redux'
 import queryString from 'query-string'
 import axios from 'axios'
+import * as Auth from 'devise-token-client'
 import ActionsType from '../constants/Actions'
 import { user } from '../api'
 
 export const signIn =
   createAction(ActionsType.User.signIn, user.signIn)
 
-export const omniauthSignIn = (params: Object) => (dispatch: Dispatch) => {
-  const baseUrl = process.env.BASE_URL
-  const authUrl = `${baseUrl}auth/${params.provider}?auth_origin_url=${encodeURIComponent('http://localhost:4000/blank')}`
-  const openAuthPopup = () => window.open(authUrl, null, 'width=800, height=600')
-  const popup = openAuthPopup()
-  setTimeout(() => {
-    const queries = queryString.parse(popup.location.search)
-    console.log(queries)
-    const callbackUrl = `${baseUrl}auth/validate_token`
-    // const callbackUrl = `${baseUrl}auth/${params.provider}/callback?resource_class=User`
+const baseAuthUrl = `${process.env.BASE_URL}auth`
 
-    axios.get(
-      callbackUrl,
-      {
-        headers: {
-          'access-token': queries.auth_token,
-          expiry: queries.expiry,
-          'token-type': 'Bearer',
-          uid: queries.uid,
-          client: queries.client_id,
-        },
-      },
-    ).then(res => console.log(res.data))
-  }, 6000)
-  // user.signIn(params).then((res) => {
-  //  console.log(res)
-  // })
+const validateTokenUrl = `${baseAuthUrl}/validate_token`
+const fetchAutheticatedUser = config => axios.get(validateTokenUrl, config)
+
+const openAuthPopup = (provider) => {
+  const authOriginUrl = encodeURIComponent(`http://${window.location.host}/blank`)
+  const authQuery = `auth_origin_url=${authOriginUrl}`
+  const omniauthUrl = `${baseAuthUrl}/${provider}?${authQuery}`
+
+  return window.open(omniauthUrl, null, 'width=800, height=600')
+}
+
+const requestHeadersFromQuery = (query) => {
+  const queries = queryString.parse(query)
+  return {
+    'access-token': queries.auth_token,
+    client: queries.client_id,
+    expiry: queries.expiry,
+    'token-type': 'Bearer',
+    uid: queries.uid,
+  }
+}
+
+export const omniauthSignIn = (params: Object) => (dispatch: Dispatch) => {
+  if (Auth.isSignedIn()) {
+    fetchAutheticatedUser({ headers: Auth.requestHeaders() })
+      .then((res) => { console.log(res.data) })
+  } else {
+    const popup = openAuthPopup(params.provider)
+    const checkDidUserAuthenticated = setInterval(() => {
+      const authHeaders = requestHeadersFromQuery(popup.location.search)
+      if (authHeaders.uid) {
+        popup.close()
+        clearInterval(checkDidUserAuthenticated)
+        fetchAutheticatedUser({ headers: authHeaders })
+          .then((res) => {
+            console.log(res.data)
+            Auth.persistToken(new Headers(res.headers))
+            if (res.date.success){
+            // Dispatch SignInSuccess action or return Resolve
+            } else {
+            // Dispatch SignInFailure action or return Reject
+            }
+          })
+      } else if (popup.closed) {
+        clearInterval(checkDidUserAuthenticated)
+        // Dispatch SignInFailure action
+      }
+    }, 1000)
+  }
 }
