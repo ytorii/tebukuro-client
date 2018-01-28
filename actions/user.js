7f79825da221a68865acd5ec3bb38539b12feca2
ytorii <metalwarrior_type_jp@hotmail.com>
2018-01-28 20:54:33 +0900

    Solved CORS exception by accessing popup resource in provider site by thrown away

--- actions/user.js
+++ actions/user.js
@@ -11,11 +11,11 @@ const validateTokenUrl = `${baseAuthUrl}/validate_token`
 const fetchAutheticatedUser = config => axios.get(validateTokenUrl, config)
 
 const openAuthPopup = (provider) => {
-  const authOriginUrl = encodeURIComponent(`http://${window.location.host}/_blank`)
-  const authQuery = `auth_origin_url=${authOriginUrl}`
+  const authQuery = `auth_origin_url=${encodeURIComponent(window.location.href)}`
   const omniauthUrl = `${baseAuthUrl}/${provider}?${authQuery}`
+  const settings = 'scrollbars=no,toolbar=no,location=no,titlebar=no,directories=no,status=no,menubar=no,width=800,height=600'
 
-  return window.open(omniauthUrl, null, 'width=800, height=600')
+  return window.open(omniauthUrl, provider, settings)
 }
 
 const requestHeadersFromQuery = (query) => {
@@ -30,7 +30,15 @@ const requestHeadersFromQuery = (query) => {
 }
 
 const checkDidUserAuthenticated = (popup, resolve, reject) => {
-  const authHeaders = requestHeadersFromQuery(popup.location.search)
+  // In order to check authentication in popup window has completed,
+  // we need to fetch search queries of popup windows address.
+  // But in authenticating in provider's domain, this check causes cors exception
+  // for secutity reason.
+  // To handle this, catch exception and throw away...
+  let authHeaders = {}
+  try {
+    authHeaders = requestHeadersFromQuery(popup.location.search)
+  } catch (err) { }
 
   if (authHeaders.uid) {
     popup.close()
@@ -45,15 +53,17 @@ const checkDidUserAuthenticated = (popup, resolve, reject) => {
   } else if (popup.closed) {
     return reject(new Error('Error has occured in User Authentication with the provider'))
   }
-  return setTimeout(() => checkDidUserAuthenticated(popup, resolve, reject), 0)
+  return setTimeout(() => checkDidUserAuthenticated(popup, resolve, reject), 100)
 }
 
 const omniauthSignIn = (params: Object) => {
   if (Auth.isSignedIn()) {
     return fetchAutheticatedUser({ headers: Auth.requestHeaders() })
-      .then(res => res.data.data, err => Promise.reject(err))
+      .then((res) => {
+        Auth.persistToken(new Headers(res.headers))
+        return res.data.data
+      }, err => Promise.reject(err))
   }
-  // const popup = openAuthPopup(params.provider)
   const popup = openAuthPopup(params.provider)
 
   return new Promise((resolve, reject) => checkDidUserAuthenticated(popup, resolve, reject))
